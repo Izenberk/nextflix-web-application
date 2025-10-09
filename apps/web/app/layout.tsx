11cf3cf229c8ea8a461e00503f6d63eab3e01a86
Izenberk <aceskorn555@outlook.com>
2025-10-09 22:52:37 +0700

    feat(web): add favicon

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Nextflix',
+  icons: {
+    icon: '/favicon.ico',
+  },
   description: 'Watch anything, anywhere — built by Korn.',
 }
 
